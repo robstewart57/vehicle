5a6dcb713903b0b5962492272190239bd5c1b0db
Rob Stewart <robstewart57@gmail.com>
2023-03-10 23:14:13 +0000

    Updates the test_mnist_custom_loss.py benchmark script

--- vehicle-python/benchmarks.sh
+++ vehicle-python/benchmarks.sh
@@ -3,16 +3,19 @@
 echo "epochs,DL,ratio,accuracy" > classification-data.csv
 echo "epochs,DL,ratio,delta,satisfaction" > constraint-data.csv
 
-for EPOCHS in 20
+export EPOCHV=10
+
+# for EPOCHS in 20
+# do
+# export EPOCHV=$EPOCHS
+for DL in LossFunction-DL2 LossFunction-Godel LossFunction-Lukasiewicz LossFunction-Product
 do
-    export EPOCHV=$EPOCHS
-    for RATIO in 100 95 90 85 80 75 70 65 60 55 50 45 40 35 30 25 20 15 10 5 0
+    export DLV=$DL
+    # for RATIO in 100 95 90 85 80 75 70 65 60 55 50 45 40 35 30 25 20 15 10 5 0
+    for RATIO in 100 90 80 70 60 50 40 30 20 10 0
     do
         export RATIOV=$RATIO
-        for DL in LossFunction-DL2 LossFunction-Godel LossFunction-Lukasiewicz LossFunction-Product
-        do
-            export DLV=$DL
-            poetry run python3 tests/test_mnist_custom_loss.py
-        done
+        poetry run python3 tests/test_mnist_custom_loss.py
     done
 done
+# done
